aadc9ccc7302d24b97772eb68a7645c46eced9df
Ben Pesoa <benpesoa@gmail.com>
2022-08-15 10:02:39 -0300

    finalizando dashboard

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,11 +1,33 @@
 var express = require('express');
 var router = express.Router();
 var novedadesModel = require('../../models/novedadesModel');
-
+var util = require('util');
+var cloudinary = require('cloudinary').v2;
+const uploader = util.promisify(cloudinary.uploader.upload);
+const destroy = util.promisify(cloudinary.uploader.destroy);
 
 router.get('/', async function(req, res, next) {
 
   var novedades = await novedadesModel.getNovedades();
+
+  novedades = novedades.map(novedad => {
+    if (novedad.id_img) {
+      const imagen = cloudinary.image(novedad.id_img, {
+        width: 100,
+        height: 100,
+        crop: 'fill'
+      });
+      return {
+        ...novedad,
+        imagen
+      }
+    } else {
+      return {
+        ...novedad,
+        imagen: ''
+      }
+    }
+  });
 
   res.render('admin/novedades', {
     layout:'admin/layout',
@@ -22,8 +44,21 @@ router.get('/agregar', (req, res, next) => {
 
 router.post('/agregar', async (req, res, next) => {
   try {
+
+    var id_img = '';
+    console.log(req.files.imagen);
+
+    if (req.files && Object.keys(req.files).length > 0) {
+      imagen = req.files.imagen;
+      id_img = (await uploader(imagen.tempFilePath)).public_id;
+    }
+
+
     if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != "") {
-      await novedadesModel.insertNovedad(req.body);
+      await novedadesModel.insertNovedad({
+        ...req.body,
+        id_img
+      });
       res.redirect('/admin/novedades')
     } else {
       res.render('admin/agregar', {
@@ -44,6 +79,12 @@ router.post('/agregar', async (req, res, next) => {
 
 router.get('/eliminar/:id', async (req, res, next) => {
   var id = req.params.id;
+
+  let novedad = await novedadesModel.getNovedadById(id);
+  if (novedad.id_img) {
+    await (destroy(novedad.id_img));
+  }
+
   await novedadesModel.deleteNovedadById(id);
   res.redirect('/admin/novedades')
 });
@@ -59,10 +100,31 @@ router.get('/editar/:id', async (req, res, next) => {
 
 router.post('/editar', async (req, res, next) => {
   try {
+
+    let id_img = req.body.img_original;
+    let borrar_img_vieja = false;
+    if (req.body.img_delete === "1") {
+      id_img = null;
+      borrar_img_vieja = true;
+    } else {
+      if (req.files && Object.keys(req.files).length > 0) {
+        imagen = req.files.imagen;
+        id_img = (await uploader(imagen.tempFilePath)).public_id;
+        borrar_img_vieja = true;
+      }
+    }
+    if (borrar_img_vieja && req.body.img_original) {
+      await (destroy(req.body.img_original));
+    }
+
+
+
+
     let obj = {
       titulo: req.body.titulo,
       subtitulo: req.body.subtitulo,
-      cuerpo: req.body.cuerpo
+      cuerpo: req.body.cuerpo,
+      id_img
     }
     console.log(obj);
 
